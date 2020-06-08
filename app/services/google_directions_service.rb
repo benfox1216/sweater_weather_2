21bb9cec0579cd33d404a742a54b33c169fa6baf
Ben Fox <benfox1216@gmail.com>
2020-06-08 09:39:09 -0600

    Add test & service to verify correct response from google directions API

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -1,17 +1,21 @@
 class GoogleDirectionsService
-  attr_reader
+  attr_reader :something
   
-  def initialize()
+  def initialize(orig_city_state, dest_city_state)
+    @orig_city = orig_city_state.split(',').first
+    @orig_state = orig_city_state.split(',').last
+    @dest_city = dest_city_state.split(',').first
+    @dest_state = dest_city_state.split(',').last
   end
   
-  def get_coords
+  def get_directions
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
   
   def response
-    uri = "maps/api/directions/outputFormat?parameters"
-    Faraday.get("http://maps.googleapis.com/#{uri}")
+    uri = "maps/api/directions/json?origin=#{@orig_city}+#{@orig_state}&destination=#{@dest_city}+#{@dest_state}&key=#{ENV['GOOGLE_API_KEY']}"
+    Faraday.get("https://maps.googleapis.com/#{uri}")
   end
 end
--- spec/services/google_geocoding_api_spec.rb
+++ spec/services/google_geocoding_api_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Google Directions API" do
   it "returns expected response", :vcr do
-    response = GoogleDirectionsService.new('denver,co')
-    # expected = ?
+    response = GoogleDirectionsService.new('denver,co', 'pueblo,co').get_directions
+    expected = "1 hour 48 mins"
 
-    expect().to eq(expected)
+    expect(response[:routes].first[:legs].first[:distance][:duration]).to eq(expected)
   end
 end
