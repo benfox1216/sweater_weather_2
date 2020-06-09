fdb21d9bc6cf9e90bf2250ddade02c179703e091
Ben Fox <benfox1216@gmail.com>
2020-06-09 09:40:26 -0600

    Add geocoding response, and with that the weather response needed

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::TrailsController < ApplicationController
   def show
+    google_geocoding_response = GoogleGeocodingService.new(params[:location])
+    coordinates = google_geocoding_response.
+      get_coords[:results].first[:geometry][:location]
+      
+    lat = coordinates[:lat]
+    lng = coordinates[:lng]
     
+    open_weather_response = OpenWeatherService.new(lat, lng)
+    weather = open_weather_response.get_weather_data[:current]
     
-    open_weather_response = OpenWeatherService.new
-    
-    binding.pry
+    temp = weather[:temp]
+    forecast = weather[:weather].first[:description]
   end
 end
