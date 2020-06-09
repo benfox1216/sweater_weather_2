7d9b912fca7dc142e6bc52212de6adffb5e13f24
Ben Fox <benfox1216@gmail.com>
2020-06-09 11:59:45 -0600

    Add more robust testing

--- spec/requests/api/v1/trails_spec.rb
+++ spec/requests/api/v1/trails_spec.rb
@@ -6,6 +6,18 @@ describe "Trails API" do
     expect(response).to be_successful
     
     json = JSON.parse(response.body, symbolize_names: true)
+    
+    expected = {
+      name: "Boulder Skyline Traverse",
+      summary: "The classic long mountain route in Boulder.",
+      difficulty: "black",
+      location: "Superior, Colorado",
+      distance_to_trail: 23.008
+    }
+    
+    expect(json[:data][:attributes][:location]).to eq("denver,co")
+    expect(json[:data][:attributes][:forecast].class).to eq(Hash)
     expect(json[:data][:attributes][:trails].count).to eq(10)
+    expect(json[:data][:attributes][:trails].first).to eq(expected)
   end
 end
