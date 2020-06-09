f072b98c9155d1e9f6d53189f4cd630534187696
Ben Fox <benfox1216@gmail.com>
2020-06-09 00:14:07 -0600

    Add ability to create a user through registration, with test passing

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+require 'securerandom'
+
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(permit_params)
+    user.api_key = SecureRandom.hex
+    
+    if user.save
+      render json: UsersSerializer.new(user), status: :created
+    else
+      # render :json {error: "passwords don't match"}, status: :bad_request
+    end
+  end
+  
+  private
+  
+  def permit_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
---
+++ spec/requests/api/v1/registration_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Registration API" do
+  it "creates a user, returns correct data", :vcr do
+    headers = {"Accept" => "application/json"}
+    
+    params = {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+    
+    post "/api/v1/users", params: params, headers: headers
+
+    expect(response.content_type).to eq 'application/json'
+    expect(response).to be_successful
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(json[:data][:attributes][:email]).to eq("whatever@example.com")
+    expect(json[:data][:attributes][:api_key]).to eq(User.last.api_key)
+  end
+end
